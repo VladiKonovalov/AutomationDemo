99d79745e5911b6d64c935dfa8f8d9d81cf7df45
vladi <vladi.konov@gmail.com>
2025-05-08 13:06:26 +0300

    add full scenario of login and wrong login

--- src/main/java/features/tests.feature
+++ src/main/java/features/tests.feature
@@ -5,13 +5,29 @@ Feature: open and test the website
     And navigate to tested website
 
 
+  Scenario Outline: try login with not existed user
+    Given press on <buttonName> button
+    When enter invalid username and password
+    Then got an error message
 
-    Scenario: try login with not existed user
-      Given press on 'Login' button
-      When enter invalid username and password
+    Examples:
+      | buttonName |
+      | 'Login'    |
 
 
-  Scenario: User check one of the categories
+  Scenario Outline: try login with existing user
+    Given press on <buttonName> button
+    When login with <username> and <password>
+    Then got a welcome text for <username>
+
+    Examples:
+      | buttonName |username|password|
+      | 'Login'    |'a'       |'a'|
+
+
+  Scenario Outline: User check one of the categories
     When the user navigates to the 'category' category page
-      |Phones|
+    Examples:
+      | category |
+      | Laptops   |
 
--- src/main/java/steps/Steps.java
+++ src/main/java/steps/Steps.java
@@ -1,39 +1,39 @@
 package steps;
 
+import dev.failsafe.internal.util.Assert;
 import io.cucumber.java.en.And;
 import io.cucumber.java.en.Given;
+import io.cucumber.java.en.Then;
 import io.cucumber.java.en.When;
 import io.github.bonigarcia.wdm.WebDriverManager;
-import org.bouncycastle.oer.its.etsi102941.Url;
+import org.apache.commons.lang.RandomStringUtils;
+import org.apache.hc.core5.util.Asserts;
+import org.openqa.selenium.Alert;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
-
 import java.io.FileInputStream;
-import java.io.FileNotFoundException;
 import java.io.IOException;
 import java.util.Properties;
 
 public class Steps {
-    Properties props = new Properties();
+    Properties props ;
     WebDriver driver ;
+    Asserts asserts;
     @io.cucumber.java.en.Given("open the browser")
     public void OpenTheBrowser() {
         WebDriverManager.chromedriver().setup();
         driver = new ChromeDriver();
         driver.manage().window().maximize();
+        props = new Properties();
         try {
             FileInputStream fis = new FileInputStream("src/main/resources/config.properties");
             props.load(fis);
         } catch (IOException e) {
             throw new RuntimeException("Could not load properties file");
         }
-
-
-
 }
-
 
     @When("the user navigates to the {string} category page")
     public void theUserNavigatesToTheCategoryCategoryPage(String category) {
@@ -57,7 +57,37 @@ public class Steps {
     }
 
     @When("enter invalid username and password")
-    public void enterInvalidUsernameAndPassword() {
+    public void enterInvalidUsernameAndPassword() throws InterruptedException{
+        String generatedString = RandomStringUtils.random(5, true, true);
+        driver.findElement(By.id("loginusername")).sendKeys("user_"+generatedString);
+        driver.findElement(By.id("loginpassword")).sendKeys("pass_"+generatedString);
+        driver.findElement(By.xpath("//button[text()='Log in']")).click();
+        Thread.sleep(1000);
+    }
 
+    @Then("got an error message")
+    public void gotAnErrorMessage() {
+        Alert alert = driver.switchTo().alert();
+        String alertText = alert.getText();
+        System.out.println("Alert: " + alertText);
+        asserts.equals( alertText.toLowerCase().contains("user does not exist") ||
+                alertText.toLowerCase().contains("wrong password"));
+        alert.accept();
+        driver.quit();
+
+    }
+
+    @When("login with {string} and {string}")
+    public void loginWithUsernameAndPassword(String username,String password)throws InterruptedException {
+        driver.findElement(By.id("loginusername")).sendKeys(username);
+        driver.findElement(By.id("loginpassword")).sendKeys(password);
+        driver.findElement(By.xpath("//button[text()='Log in']")).click();
+        Thread.sleep(1000);
+    }
+
+    @Then("got a welcome text for {string}")
+    public void gotAWelcomeTextForUsername(String username) {
+        String welcomeMessege =driver.findElement(By.id("nameofuser")).getText();
+        asserts.equals( welcomeMessege.toLowerCase().contains("welcome "+username) );
     }
 }
